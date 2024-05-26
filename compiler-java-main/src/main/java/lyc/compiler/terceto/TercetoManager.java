27e9d90c702d90ba5a1a2e1f6d57bc8365ccfc71
GianSimonetti <simonetti698@gmail.com>
2024-05-26 16:30:58 -0300

    Desarrollo de elementos
     - PLUS y ASSIG funcionando
    - SI y MIENTRAS funcionan a medias, faltan los saltos del BI, pero al menos la logica va bien.

--- compiler-java-main/src/main/java/lyc/compiler/terceto/TercetoManager.java
+++ compiler-java-main/src/main/java/lyc/compiler/terceto/TercetoManager.java
@@ -28,7 +28,9 @@ public class TercetoManager {
                 case "BGT":
                 case "BLT":
                 case "BGE":
-                    lista.add(new Terceto("CMP", null, null));
+                    Integer aux1CMP = lista.size();
+                    Integer aux2CMP = lista.size()-1;
+                    lista.add(new Terceto("CMP", "["+aux1CMP.toString()+"]", "["+aux2CMP.toString()+"]"));
                     lista.add(new Terceto(elemento, null, "#"));
                     pila.add(lista.size());
                     break;
@@ -47,29 +49,15 @@ public class TercetoManager {
                 case "SINGLE":
                     pilaM.add(elemento);
                     break;
-                case "CONTARPRIMOS":
-                    while (!expresionesPrimos.isEmpty()) {
-                        String valor = expresionesPrimos.pop();
-
-                        // Lógica para verificar si la expresión es primo
-                        lista.add(new Terceto("2", null, null)); // Inicializar el divisor a 2
-                        lista.add(new Terceto(valor, null, null)); // Etiqueta para el bucle
-                        lista.add(new Terceto("DIV", "ant2", "ant2")); // Calcular el módulo de la expresión y 2
-                        lista.add(new Terceto("BNE", null, "#")); // Saltar si el módulo no es igual a 0
-                        lista.add(new Terceto("ADD", "1", null)); // Incrementar el contador de divisores
-                        lista.add(new Terceto("ADD", "1", null)); // Incrementar el divisor
-                        lista.add(new Terceto("LT", null, expresion)); // Comparar si el divisor es menor que la expresión
-                        lista.add(new Terceto("BNE", null, "#LABEL")); // Saltar si el divisor es menor que la expresión
-                        lista.add(new Terceto("EQ", "0", null)); // Verificar si el contador de divisores es igual a 0 (es primo)
-                        lista.add(new Terceto("BNE", null, "#")); // Saltar si no es primo
-                        contadorPrimos++; // Incrementar el contador de primos
-                        pila.add(lista.size()); // Agregar la posición actual de la lista a la pila
-                    }
-                    break;
                 case "ASSIG":
                     Integer aux1 = lista.size();
                     Integer aux2 = lista.size()-1;
                     lista.add(new Terceto(":=", "["+aux1.toString()+"]", "["+aux2.toString()+"]"));
+                    break;
+                case "PLUS":
+                    Integer auxPlus = lista.size();
+                    Integer aux2Plus = lista.size()-1;
+                    lista.add(new Terceto("+", "["+auxPlus.toString()+"]", "["+aux2Plus.toString()+"]"));
                     break;
                 default:
                     lista.add(new Terceto(elemento, null, null));
